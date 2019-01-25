53ef1e50a6721c9d09d36f2a075afd9ea4622d41
Taylor Radtke <taylorradtke@gmail.com>
2019-01-25 17:14:27 -0500

    tidy

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,22 @@
-const hand__second  = document.querySelector('.hand__second');
-const hand__minute  = document.querySelector('.hand__minute');
-const hand__hour    = document.querySelector('.hand__hour');
+const handSecond = document.querySelector(".hand__second");
+const handMinute = document.querySelector(".hand__minute");
+const handHour = document.querySelector(".hand__hour");
 
 function setDate() {
-  const time__now     = new Date();
-  const time__seconds = time__now.getSeconds();
-  const time__minutes = time__now.getMinutes();
-  const time__hours   = time__now.getHours();
+	const timeNow = new Date();
+	const timeSeconds = timeNow.getSeconds();
+	const timeMinutes = timeNow.getMinutes();
+	const timeHours = timeNow.getHours();
 
-  // We are adding 90 here to offset the inital
-  // css setting of transform: rotate(90deg)
-  const degrees__seconds = ((time__seconds / 60) * 360) + 90;
-  const degrees__minutes = ((time__minutes / 60) * 360) + 90;
-  const degrees__hours   = ((time__hours / 12)   * 360) + 90;
+	// We are adding 90 here to offset the inital
+	// css setting of transform: rotate(90deg)
+	const degreesSeconds = (timeSeconds / 60) * 360 + 90;
+	const degreesMinutes = (timeMinutes / 60) * 360 + 90;
+	const degreesHours = (timeHours / 12) * 360 + 90;
 
-  hand__second.style.transform = `rotate(${degrees__seconds}deg)`;
-  hand__minute.style.transform = `rotate(${degrees__minutes}deg)`;
-  hand__hour.style.transform   = `rotate(${degrees__hours}deg)`;
+	handSecond.style.transform = `rotate(${degreesSeconds}deg)`;
+	handMinute.style.transform = `rotate(${degreesMinutes}deg)`;
+	handHour.style.transform = `rotate(${degreesHours}deg)`;
 }
 
 setInterval(setDate, 1000);
